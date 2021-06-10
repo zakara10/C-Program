e78e805643f1317ee25f6c13bdd2c107b7742915
Roshan Kumar <76031173+zakara10@users.noreply.github.com>
2021-06-10 13:43:15 +0530

    A number is positive or negative

---
+++ demo5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int x;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    if(x>0)
+        printf("Positive Number ");
+    else
+        printf("Negative Number ");4
+
+}
